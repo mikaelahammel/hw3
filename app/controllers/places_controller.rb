3af1e0ed54f61a656d7abac9a9246936f01b81a5
Mikaela Hammel <mikaela.hammel@kellogg.northwestern.edu>
2023-05-17 03:18:29 +0000

    working on hw

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,31 @@
 class PlacesController < ApplicationController
+
+  def index
+    @places = Place.all 
+  end
+  
+  def show
+    @place = Place.find_by({"id" => params["id"] })
+    # think I need to put posts below but I'm not sure
+    @posts = Post.where({"place_id" => @place["id"] })
+  end
+
+  def new
+    @place = Place.new
+  end
+  
+  def create
+    # start with a new Place
+    @place = Place.new
+
+    # assign user-entered form data to Place's columns
+    @place["name"] = params["place"]["name"]
+
+    # save Place row
+    @place.save
+
+    # redirect user
+    redirect_to"/places"
+  end
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,34 @@
 class PostsController < ApplicationController
+
+  def show
+    @post = Post.find_by({"id" => params["id"]})
+    @place = Place.find_by({"id" => params["id"]})
+    # render contacts/show view with details about Contact
+  end
+
+  def new
+    @post = Post.new
+    @post["place_id"] = params["id"]
+  end
+
+  def create
+    # start with a new Post
+    @post = Post.new
+    
+
+    # assign user-entered form data to Post's columns
+    @post["title"] = params["post"]["title"]
+    @post["description"] = params["post"]["description"]
+    @post["posted_on"] = params["post"]["posted_on"]
+
+    # assign relationship between Post and Place
+    @post["place_id"] = params["post"]["place_id"]
+
+    # save Post row
+    @post.save
+
+    # redirect user
+    # not sure about this one
+    redirect_to"/places/#{@post["place_id"]}"
+  end
 end
