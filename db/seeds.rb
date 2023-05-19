9df77537ee7257189c8179e80221645d0a6b30ea
Mikaela Hammel <mikaela.hammel@kellogg.northwestern.edu>
2023-05-19 16:07:13 +0000

    moving towards the finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-places = Place.create([{name: "Mexico City"}, {name: "Charleston"}, {name: "Beijing"}, {name: "Amsterdam"} ])
+places = Place.create([{name: "Mexico City"}, {name: "Charleston"}, {name: "Beijing"}])
+posts = Post.create([
+  {title: "Ate tacos", description: "Tacos are delicious and these were no expection", posted_on: "2023-03-14", place_id: "1"}, 
+  {title: "Ate pizza", description: "Pizza isn't as good as tacos", posted_on: "2023-04-19", place_id: "2"},
+  {title: "Went to a museum", description: "Decided to mix it up today", posted_on: "2023-05-17", place_id: "3"}])
