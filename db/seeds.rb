76144e165183fd15f3ff3e6fdd0468304a1a0625
Mikaela Hammel <mikaela.hammel@kellogg.northwestern.edu>
2023-05-19 16:00:51 +0000

    moving forward

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+places = Place.create([{name: "Mexico City"}, {name: "Charleston"}, {name: "Beijing"}, {name: "Amsterdam"} ])
